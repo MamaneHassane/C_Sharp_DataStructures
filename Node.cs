a67eba806377bdb7634870e3a53f566e3dba8e13
MamaneHassane <hassane.mamane017@gmail.com>
2024-07-28 11:59:15 +0100

    Ajoutez des fichiers projet.

---
+++ Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_DataStructures
+{
+    class Node<T> (T value) 
+    {
+
+        // La valeur dans le noeud
+        private T? Value = value;
+        // Le noeud suivant
+        private Node<T>? Next { get; set; }
+
+        // Fonction pour afficher le noeud
+        public override string ToString()
+        {
+            return $"{Value} --> ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using C_Sharp_DataStructures;
+
+Console.WriteLine("Hello, World!");
+
+Node<string> node = new("Hassane");
+
+Console.WriteLine(node);
